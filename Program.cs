ad84d7d04ddb98dc6f8e0fdbd4296477e096134d
its julii <nanji828@gmail.com>
2022-12-08 18:52:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Birthday_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rent = double.Parse(Console.ReadLine());
+
+            double cake = rent * 0.2;
+            double drinks = cake - (cake * 0.45);
+            double man = rent / 3; 
+
+             Console.WriteLine (rent + cake + drinks + man);
+            
+
+
+
+        }
+    }
+}
